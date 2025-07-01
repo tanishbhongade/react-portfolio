4c1aa6d9bb4ddc2190810197936bfc474d305dd4
Tanish Bhongade <tanishbhongade@gmail.com>
2025-07-01 16:20:37 +0530

    Added loader animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,34 @@
 import Navbar from "./Navbar";
 import ProjectSection from "./ProjectSection";
+import { useState, useEffect } from "react";
+import { RingLoader } from "react-spinners";
 
 function Projects() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 1000); // 0.5 sec
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div>
       <Navbar />
-      <ProjectSection />
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+            width: "100vw"
+          }}
+        >
+          <RingLoader color="#fff" size={100} />
+        </div>
+      ) : (
+        <div>
+          <ProjectSection />
+        </div>
+      )}
     </div>
   );
 }
